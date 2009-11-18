01c30f1da2fd80c9c70b2ed43a7b8dff0ee03e37
Hugo Barauna <hugo@plataformatec.com.br>
2009-11-17 23:32:57 -0200

    Just replacing tabs with two spaces

--- src/parser.c
+++ src/parser.c
@@ -16,35 +16,35 @@ int expr(BufferedInputStream *stream)
     switch (current_state) {
     case 0:
       if (token->class == ID || token->class == NUMBER)
-	      current_state = 1;
+        current_state = 1;
       else if (token->class == LPAR)
-	      current_state = 2;
+        current_state = 2;
       else
-	      return 0;
+        return 0;
       token = next_token(stream);
       break;
     case 1:
       if (token->class == MULT || token->class == ADD)
-	      current_state = 0;
+        current_state = 0;
       else
-	      return 1;
+        return 1;
       token = next_token(stream);
       break;
     case 2:
       printf("called expr() submachine\n");
       if (!expr(stream))
-	      return 0;
+        return 0;
       printf("return from expr()\n");
       current_state = 3;
       continue;
     case 3:
       printf("value: %s", token->value);
       if (token->class == RPAR) {
-	      printf("entered here");
-	      current_state = 1;
+        printf("entered here");
+        current_state = 1;
       }
       else
-	      return 0;
+        return 0;
       token = next_token(stream);
       break;
     }
@@ -59,15 +59,15 @@ int stmt(BufferedInputStream *stream)
     switch (current_state) {
     case 0:
       if (token->class == IF)
-	      current_state = 1;
+        current_state = 1;
       else if (token->class == WHILE)
-	      current_state = 3;
+        current_state = 3;
       else {
-	      printf("stmt called expr()\n");
-	      if (!expr(stream)) return 0;
-	      printf("returned from expr() to stmt\n");
-	      current_state = 2;
-	      continue;
+        printf("stmt called expr()\n");
+        if (!expr(stream)) return 0;
+        printf("returned from expr() to stmt\n");
+        current_state = 2;
+        continue;
       }
       token = next_token(stream);
       break;
@@ -87,41 +87,41 @@ int stmt(BufferedInputStream *stream)
       continue;
     case 4:
       if (token->class == DO)
-	      current_state = 6;
+        current_state = 6;
       else return 0;
         token = next_token(stream);
       break;
     case 5:
       if (token->class == DO)
-	      current_state = 7;
+        current_state = 7;
       else return 0;
         token = next_token(stream);
       break;
     case 6:
       printf("called stmt()\n");
       if (!stmt(stream))
-	      return 0;
+        return 0;
       printf("return from stmt()\n");
       current_state = 8;
       break;
     case 7:
       printf("called stmt()\n");
       if (!stmt(stream))
-	      return 0;
+        return 0;
       printf("return from stmt()\n");
       current_state = 9;
       break;
     case 8:
       if (token->class == ELSE)
-	      current_state = 7;
+        current_state = 7;
       else if (token->class == END)
-	      current_state = 2;
+        current_state = 2;
       else return 0;
       token = next_token(stream);
       break;
     case 9:
       if (token->class == END)
-	      current_state = 2;
+        current_state = 2;
       else return 0;
       token = next_token(stream);
       break;
@@ -161,16 +161,16 @@ int or(BufferedInputStream *stream)
     switch (current_state) {
     case 0:
       if (token->class == INCREMENT || token->class == DECREMENT)
-	      current_state = 1;
+        current_state = 1;
       else if (token->class == ID || token->class == NUMBER)
-	      current_state = 2;
+        current_state = 2;
       else
-	      return 2;
+        return 2;
       token = next_token(stream);
       break;
     case 1:
       if (token->class == ID || token->class == NUMBER)
-	      current_state = 2;
+        current_state = 2;
       else return 0;
         token = next_token(stream);
       break;
@@ -178,22 +178,22 @@ int or(BufferedInputStream *stream)
       switch (token->class) {
       case INCREMENT:
       case DECREMENT:
-	      current_state = 2;
-	      token = next_token(stream);
-	      break;
+        current_state = 2;
+        token = next_token(stream);
+        break;
       case LBRA:
-	      current_state = 3;
-	      token = next_token(stream);
-	      break;
+        current_state = 3;
+        token = next_token(stream);
+        break;
       case LPAR:
-	      current_state = 4;
-	      token = next_token(stream);
-	      break;
+        current_state = 4;
+        token = next_token(stream);
+        break;
       case DOT:
       case TO:
-	      current_state = 5;
-	      token = next_token(stream);
-	      break;
+        current_state = 5;
+        token = next_token(stream);
+        break;
       case LOGIC_AND:
       case LOGIC_OR:
       case PWR:
@@ -212,11 +212,11 @@ int or(BufferedInputStream *stream)
       case MOD:
       case ADD:
       case SUB:
-	      current_state = 0;
-	      token = next_token(stream);
-	      break;
+        current_state = 0;
+        token = next_token(stream);
+        break;
       default:
-	      return 1;
+        return 1;
       }
       break;
     case 3:
@@ -228,31 +228,31 @@ int or(BufferedInputStream *stream)
       break;
     case 4:
       if (token->class == RPAR)
-	      current_state = 2;
+        current_state = 2;
       else {
-	      printf("called arg_list()\n");
-	      arg_list(stream);
-	      printf("returned from arg_list()\n");
-	      current_state = 7;
-	      continue;
+        printf("called arg_list()\n");
+        arg_list(stream);
+        printf("returned from arg_list()\n");
+        current_state = 7;
+        continue;
       }
       token = next_token(stream);
       break;
     case 5:
       if (token->class == ID)
-	      current_state = 2;
+        current_state = 2;
       else return 0;
       token = next_token(stream);
       break;
     case 6:
       if (token->class == RBRA)
-	      current_state = 2;
+        current_state = 2;
       else return 0;
       token = next_token(stream);
       break;
     case 7:
       if (token->class == RPAR)
-	      current_state = 2;
+        current_state = 2;
       else return 0;
       token = next_token(stream);
       break;
